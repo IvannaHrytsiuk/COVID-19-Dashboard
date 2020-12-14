45c353bce7b2faac8ad5355800474439e6d68bb8
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-15 00:30:25 +0300

    feat: move async function in Store

--- map-block/src/js/index.js
+++ map-block/src/js/index.js
@@ -8,54 +8,14 @@ import {
 } from './constants/constants';
 import { ViewMapClass } from './View/index';
 import { ControllerClass } from './Controller/index';
+import { StoreClass } from './Store';
 
-let cases = [];
-let countryName = [];
-let codeName = [];
-let dataAll;
 let nameCountry;
-let p;
-const countryUrl = [];
-
-async function sendCountry() {
-  try {
-    const url = `https://api.covid19api.com/summary`;
-    const res = await fetch(url);
-    const data = await res.json();
-    if (data.Message === '') {
-      countryName = data.Countries.map((e) => e.Country);
-      codeName = data.Countries.map((e) => e.CountryCode);
-      cases = data.Countries.map((e) => e.TotalConfirmed);
-
-      for (let i = 0; i < countryName.length; i += 1) {
-        const urlMapCenter = `https://api.mapbox.com/geocoding/v5/mapbox.places/${countryName[i]},${codeName[i]}.json?types=country&access_token=pk.eyJ1IjoibXlmZW5peDkyIiwiYSI6ImNrYXBpdXhwMTF5NTYzMXA2emY0M3pnd24ifQ.I73eBezMUvPr3OAN-aF1Cg`;
-        const resMapCenter = await fetch(urlMapCenter);
-        const dataMapCenter = await resMapCenter.json();
-        countryUrl.push(dataMapCenter.features[0].center);
-        // доделать
-        // p = new Promise((resolve, reject) => {
-        //   resolve(fetch(urlMapCenter));
-        // });
-      }
-      // await Promise.all(Array.from(p)).then((val) => {
-      //   console.log(val);
-      // });
-      ViewMap.addCircle();
-    } else {
-      throw Error(data.Message);
-    }
-  } catch (error) {
-    console.log(error);
-  } finally {
-    dataAll = Object.assign(...codeName.map((n, i) => ({ [n]: cases[i] })));
-  }
-}
-
+const Store = new StoreClass();
 const ViewMap = new ViewMapClass();
 
 grayscale.addTo(map);
 info.onAdd = ViewMap.addInfo;
-info.update = ViewMap.updateInfo;
 info.addTo(map);
 
 // get color depending on population density value
@@ -88,7 +48,6 @@ function onEachFeature(feature, layer) {
       const layers = e.target;
       nameCountry = layers.feature.properties.name;
     },
-  //  click: zoomToFeature,
   });
 }
 
@@ -100,11 +59,15 @@ const geojson = L.geoJson(countryData, {
 legend.onAdd = ViewMap.addLegend;
 legend.addTo(map);
 
+// document.querySelector('body').addEventListener('click', () => {
+//   console.log(nameCountry);
+// //  Store.sendCountry()
+// })
+
 document.addEventListener('DOMContentLoaded', () => {
+  Store.sendCountry();
   ViewMap.init();
-  sendCountry();
+  setTimeout(ViewMap.addCircle, 1000);
 });
 
-export {
-  dataAll, geojson, countryUrl, cases,
-};
+export { geojson };
