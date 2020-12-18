9c5aaf3442ec01ce54ba6c156082a4f50121d7a7
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-19 00:09:13 +0300

    fix: change name for selector

--- src/scripts/graphic-block/Controller/controller.js
+++ src/scripts/graphic-block/Controller/controller.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/prefer-default-export */
-export const ControllerClass = class {
+export const ControllerGraphicClass = class {
     constructor() {
         this.chooseValue;
     }
 
     changeSelect() {
-        this.index = document.querySelector('#chooseOptionsGraphic').options.selectedIndex;
-        this.chooseValue = document.querySelector('#chooseOptionsGraphic').options[this.index].value;
+        this.index = document.querySelector('.graphic').options.selectedIndex;
+        this.chooseValue = document.querySelector('.graphic').options[this.index].value;
         return this.chooseValue;
     }
 };
