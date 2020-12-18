fc563c44e9bb610903dfdba4e812f39f193bb159
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-19 00:10:26 +0300

    feat: change ModelClass

--- src/scripts/graphic-block/Model/model.js
+++ src/scripts/graphic-block/Model/model.js
@@ -1,43 +1,70 @@
 /* eslint-disable import/prefer-default-export */
-import { ControllerClass } from '../Controller/index';
+import { ControllerGraphicClass } from '../Controller/index';
 import { ViewGraphicClass } from '../View/index';
-import { allDayCases, allDaydDeaths, allDayRecovered } from '../../state';
+import {
+    allDayCases, allDaydDeaths, allDayRecovered, newDay,
+} from '../../state';
+import { State } from '../../../index';
 
-const Controller = new ControllerClass();
+const Controller = new ControllerGraphicClass();
 const ViewGraphic = new ViewGraphicClass();
 
 export const ModelClass = class {
     constructor() {
-        this.apiName = 'Total Cases';
+        this.apiName = 'Total confirmed';
     }
 
     changeColorCircle() {
         this.select = Controller.changeSelect();
         switch (this.select) {
-        case 'total_case':
-            this.apiName = 'Total Cases';
+        case 'Total confirmed':
+            this.apiName = 'Total confirmed';
             ViewGraphic.init(this.apiName, allDayCases, 'red');
             break;
-        case 'total_death':
-            this.apiName = 'Total Death';
-            ViewGraphic.init('Total Death', allDaydDeaths, 'yellow');
+        case 'Total death':
+            this.apiName = 'Total death';
+            ViewGraphic.init('Total death', allDaydDeaths, 'yellow');
             break;
-        case 'total_recovery':
-            this.apiName = 'Total Recodeved';
+        case 'Total recovered':
+            this.apiName = 'Total recodeved';
             ViewGraphic.init(this.apiName, allDayRecovered, 'lightblue');
             break;
-        // case 'new_case':
-        //     this.apiName = 'NewConfirmed';
-        //     ViewGraphic.init(this.apiName, 'red', 0.4);
-        //     break;
-        // case 'new_death':
-        //     this.apiName = 'NewDeaths';
-        //     ViewGraphic.init(this.apiName, 'yellow', 0.005);
-        //     break;
-        // case 'new_recovery':
-        //     this.apiName = 'NewRecovered';
-        //     ViewGraphic.init(this.apiName, 'lightblue', 0.3);
-        //     break;
+        case 'New confirmed':
+            this.apiName = 'New confirmed';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDayCases), 'red');
+            break;
+        case 'New death':
+            this.apiName = 'New death';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDaydDeaths), 'yellow');
+            break;
+        case 'New recovered':
+            this.apiName = 'New recovered';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDayRecovered), 'lightblue');
+            break;
+        case 'Total confirmed 100K':
+            this.apiName = 'Total confirmed 100K';
+            ViewGraphic.init('Total confirmed 100K', allDayCases, 'red');
+            break;
+        case 'Total death 100K':
+            this.apiName = 'Total death 100K';
+            ViewGraphic.init('Total Death  100K', allDaydDeaths, 'yellow');
+            break;
+        case 'Total recovered 100K':
+            this.apiName = 'Total recodeved 100K';
+            ViewGraphic.init(this.apiName, allDayRecovered, 'lightblue');
+            break;
+        case 'New confirmed 100K':
+            this.apiName = 'New confirmed 100K';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDayCases), 'red');
+            break;
+        case 'New death 100K':
+            this.apiName = 'New death 100K';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDaydDeaths), 'yellow');
+            break;
+        case 'New recovered 100K':
+            this.apiName = 'New recovered 100K';
+            ViewGraphic.init(this.apiName, State.getNewEveryDayData([], allDayRecovered), 'lightblue');
+            break;
         default:
             break;
         }
