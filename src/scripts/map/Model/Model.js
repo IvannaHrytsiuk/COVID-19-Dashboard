f22e79bb4965c26bd540ca55f445e294279a8a36
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-20 02:05:40 +0300

    feat: add cases for 100K for map

--- src/scripts/map/Model/Model.js
+++ src/scripts/map/Model/Model.js
@@ -33,6 +33,30 @@ export const ModelClass = class {
             this.apiName = 'NewRecovered';
             ViewMap.addCircle(this.apiName, 'lightblue', 0.3);
             break;
+        case 'Total death 100K':
+            this.apiName = 'TotalDeaths100K';
+            ViewMap.addCircle(this.apiName, 'yellow', 0.003);
+            break;
+        case 'Total confirmed 100K':
+            this.apiName = 'TotalConfirmed100K';
+            ViewMap.addCircle(this.apiName, 'red', 0.2);
+            break;
+        case 'Total recovered 100K':
+            this.apiName = 'TotalRecovered100K';
+            ViewMap.addCircle(this.apiName, 'lightblue', 0.15);
+            break;
+        case 'New confirmed 100K':
+            this.apiName = 'NewConfirmed100K';
+            ViewMap.addCircle(this.apiName, 'red', 0.003);
+            break;
+        case 'New death 100K':
+            this.apiName = 'NewDeaths100K';
+            ViewMap.addCircle(this.apiName, 'yellow', 0.0001);
+            break;
+        case 'New recovered 100K':
+            this.apiName = 'NewRecovered100K';
+            ViewMap.addCircle(this.apiName, 'lightblue', 0.004);
+            break;
         default:
             break;
         }
