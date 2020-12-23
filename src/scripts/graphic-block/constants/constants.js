766a75b4bcc54548752b9d56c3a80ec35cfec1be
Yana Kornejkova <bloom.hog@gmail.com>
2020-12-23 20:00:56 +0300

    feat: add options for chart

--- src/scripts/graphic-block/constants/constants.js
+++ src/scripts/graphic-block/constants/constants.js
@@ -4,6 +4,7 @@ import 'chart.js/dist/Chart.css';
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
+    maintainAspectRatio: false,
     options: {
         responsive: true,
         scales: {
